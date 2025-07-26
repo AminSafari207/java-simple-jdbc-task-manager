a6d9c5d735c6baceb9f9360d6aefd1778b6ba3c3
AminSafari207 <safarin207368@gmail.com>
2025-07-26 15:48:45 +0330

    Implement batch & non-batch createEmployee method for employee jdbc service

---
+++ src/java/service/EmployeeService.java
@@ -0,0 +1,88 @@
+package service;
+
+import model.Employee;
+import model.EmployeeId;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeService {
+    public Employee createEmployee(Employee employee) {
+        String sqlQuery = "INSERT INTO employee (first_name, last_name, email) VALUES (?, ?, ?)";
+
+        try (
+                Connection connection = DBConnection.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS);
+        ) {
+            String firstName = employee.getFirstName();
+            String lastName = employee.getLastName();
+            String email = employee.getEmail();
+
+            ps.setString(1, firstName);
+            ps.setString(2, lastName);
+            ps.setString(3, email);
+            ps.executeUpdate();
+
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    return new Employee(
+                            new EmployeeId(rs.getInt(1)),
+                            firstName,
+                            lastName,
+                            email
+                    );
+                } else {
+                    throw new SQLException("Failed to retrieve generated employee ID.");
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Employee> createEmployee(List<Employee> employees) {
+        String sqlQuery = "INSERT INTO employee (first_name, last_name, email) VALUES (?, ?, ?)";
+        List<Employee> createdEmployees = new ArrayList<>();
+
+        try (
+                Connection connection = DBConnection.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS)
+        ) {
+            for (Employee employee : employees) {
+                String firstName = employee.getFirstName();
+                String lastName = employee.getLastName();
+                String email = employee.getEmail();
+
+                ps.setString(1, firstName);
+                ps.setString(2, lastName);
+                ps.setString(3, email);
+                ps.addBatch();
+            }
+
+            ps.executeBatch();
+
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                int i = 0;
+                while (rs.next()) {
+                    Employee employeeWithId = new Employee(
+                            new EmployeeId(rs.getInt(1)),
+                            employees.get(i).getFirstName(),
+                            employees.get(i).getLastName(),
+                            employees.get(i).getEmail()
+                    );
+
+                    createdEmployees.add(employeeWithId);
+                    i++;
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating employees in batch", e);
+        }
+
+        return createdEmployees;
+    }
+
+
+}
