0a4ca676ba035806aba9fe4747aca559f1069353
AminSafari207 <safarin207368@gmail.com>
2025-07-26 13:07:27 +0330

    Implement Task model class

---
+++ src/java/model/Task.java
@@ -0,0 +1,66 @@
+package model;
+
+import utils.ValidationUtils;
+
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private int estimatedHours;
+
+    public Task(String title, String description, int estimatedHours) {
+        ValidationUtils.validateString("title", title);
+        ValidationUtils.validateString("description", description);
+        validateEstimatedTime(estimatedHours);
+
+        this.title = title;
+        this.description = description;
+        this.estimatedHours = estimatedHours;
+    }
+
+    public Task(int id, String title, String description, int estimatedHours) {
+        ValidationUtils.validateId(id);
+        ValidationUtils.validateString("title", title);
+        ValidationUtils.validateString("description", description);
+        validateEstimatedTime(estimatedHours);
+
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.estimatedHours = estimatedHours;
+    }
+
+    private void validateEstimatedTime(int estimatedHours) {
+        if (estimatedHours < 1 || estimatedHours > 48) {
+            throw new IllegalArgumentException("estimatedHours must be between 1 and 48.");
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getEstimatedHours() {
+        return estimatedHours;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setEstimatedHours(int estimatedHours) {
+        this.estimatedHours = estimatedHours;
+    }
+}
