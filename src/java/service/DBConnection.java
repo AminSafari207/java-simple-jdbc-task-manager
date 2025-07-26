77fcf5f0bcbeee6ad8d7070e1b5e5ab79e3c94b8
AminSafari207 <safarin207368@gmail.com>
2025-07-26 14:26:04 +0330

    Implement DBConnection jdbc service class

---
+++ src/java/service/DBConnection.java
@@ -0,0 +1,15 @@
+package service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String url = "jdbc:postgresql://localhost:5432/bank_app";
+    private static final String user = "postgres";
+    private static final String pass = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, pass);
+    }
+}
